db77ac62dbc5d2d753521169e40f6c4f394b438f
Kostas Karachalios <vrinek@hey.com>
2023-03-03 19:12:25 +0100

    Render cards and their relations

--- src/routes/Counter.svelte
+++ src/routes/Counter.svelte
@@ -2,30 +2,13 @@
 	import { onMount } from 'svelte';
 	import { Network } from 'vis-network';
 	import { DataSet } from 'vis-data';
+	import cards from './cards.js'
+	import relations from './relations.js'
 
-	// create an array with nodes
-	const nodes = new DataSet([
-		{ id: 1, label: 'Node 1' },
-		{ id: 2, label: 'Node 2' },
-		{ id: 3, label: 'Node 3' },
-		{ id: 4, label: 'Node 4' },
-		{ id: 5, label: 'Node 5' }
-	]);
-
-	// create an array with edges
-	const edges = new DataSet([
-		{ from: 1, to: 3 },
-		{ from: 1, to: 2 },
-		{ from: 2, to: 4 },
-		{ from: 2, to: 5 },
-		{ from: 3, to: 3 }
-	]);
-
-	// create a network
 	let container;
 	const data = {
-		nodes: nodes,
-		edges: edges
+		nodes: new DataSet(cards),
+		edges: new DataSet(relations),
 	};
 	const options = {};
 
